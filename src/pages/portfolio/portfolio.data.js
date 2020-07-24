02e9a3f5b0aba242af36e446e451d312c313ea07
Lev-TS <57505924+Lev-TS@users.noreply.github.com>
2020-07-24 22:28:41 +0300

    added new project

--- src/pages/portfolio/portfolio.data.js
+++ src/pages/portfolio/portfolio.data.js
@@ -1,7 +1,7 @@
 const PORTFOLIO_DATA = [
   
   {
-    "id": 6,
+    "id": 7,
     "title": "Portfolio",
     "description": 'This project is the multi-page web site that you are currently visiting. Some of my key objectives were to make it responsive, practice the React Router Dom, and create an API that serves "Contact Me" page',
     "image": "/project-images/portfolio.png",
@@ -14,7 +14,7 @@ const PORTFOLIO_DATA = [
     "sourceUrl": "https://github.com/Lev-TS/portfolio"
   },
   {
-    "id": 5,
+    "id": 6,
     "title": "Job Listing Filter",
     "description": "This project was a challange from Frontend Mentor. It's a platform that enables you to practice building websites to a design and project brief. This challange included mobile and desktop designs. The goal was to build the feature that filters out job listings based on the categories selected.",
     "image": "/project-images/job-listing.png",
@@ -27,7 +27,7 @@ const PORTFOLIO_DATA = [
     "sourceUrl": "https://github.com/Lev-TS/job-listings"
   },
   {
-    "id": 4,
+    "id": 5,
     "title": "Persistent API",
     "description": "In this capstone project for the Codecademy WEB Developer Career Path, I have built all of the routing and database logic for an internal tool of a fictional comic book publishing company. The front end and testing suite was provided by CodeCademy.",
     "image": "/project-images/backend.png",
@@ -40,7 +40,7 @@ const PORTFOLIO_DATA = [
     "sourceUrl": "https://github.com/Lev-TS/persistent-api"
   },
   {
-    "id": 3,
+    "id": 4,
     "title": "Boss Machine",
     "description": "This is a Codecademy course project, where I have created an entire API to serve information to a management application. The front end, testing suite and database logic was provided by CodeCademy.",
     "image": "/project-images/backend.png",
@@ -53,7 +53,7 @@ const PORTFOLIO_DATA = [
     "sourceUrl": "https://github.com/Lev-TS/boss-machine"
   },
   {
-    "id": 2,
+    "id": 3,
     "title": "Search Bar",
     "description": "This project was a part of a course on Zero To Mastery Academy. The goal was to learn the basic concepts of React, including lifecycle methods.",
     "image": "/project-images/monsters-rolodex.png",
@@ -64,6 +64,19 @@ const PORTFOLIO_DATA = [
     "tools": ["React", "API"],
     "liveUrl": "https://lev-ts.github.io/monsters-rolodex/",
     "sourceUrl": "https://github.com/Lev-TS/monsters-rolodex"
+  },
+  {
+    "id": 2,
+    "title": "Jamming",
+    "description": "This was a capstone project of Codecademy React course. The app allows users to connect to their Spotify account, search songs, create a playlists, and add desired songs to this playlist",
+    "image": "/project-images/jamming.png",
+    "stackType": "Frontend",
+    "projectType": "Course Work",
+    "postedAt": "Apr 2020",
+    "languages": ["HTML", "CSS", "JS"],
+    "tools": ["React", "API"],
+    "liveUrl": "http://lev-ts.surge.sh",
+    "sourceUrl": "https://github.com/Lev-TS/jamming"
   },
   {
     "id": 1,
